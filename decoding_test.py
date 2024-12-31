18c88bcb50362dbabd3cec4eb3f69df7514cf1d2
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-12-30 23:45:04 -0500

    update the thing to update reliability

--- decoding_test.py
+++ decoding_test.py
@@ -58,88 +58,91 @@ df = pd.DataFrame(
 
 
 for question in questions:
+    
     ROUND = "Preliminary Round 1"
+    
     assistant_prompt = (
-        """Fill out year from the contest name, and level from the level name.
-
-TOPIC: Choose among the following options
-'Daily Life', 'History', 'Literature', 'Mythology', 'Geography', 'Grammar Forms', 'Grammar Usage', 'Grammar Derivatives', 'Grammar Vocab', 'Grammar Listening Comprehension'
-
------
-Here are formatted EXAMPLE QUESTIONS: 
-From what Latin verb with what meaning are the English words “adroitCC” “incorrigibleCC” and “regiment” derived?, REGŌ (REGERE ) = (TO / I) RULE, From what Latin verb with what meaning is “surrogate” derived?, ROGŌ (ROGĀRE) = (TO / I) ASK, From what Latin verb with what meaning is “risible” derived?, RĪDEŌ (RĪDĒRE) = (TO / I) LAUGH, Grammar Derivatives, YEAR, CONTEST, LEVEL
-
-“Swan-shaped” and “grey-haired” describe what group of women CC sisters of the Gorgons who are most famous for sharing a single eye and tooth between themselves?, "GRAEAE // GRAY SISTERS / WOMEN", The Graeae were the children of what couple CC who were also the parents of Echidna and Ladon?, "PHORCYS and CETO, Give the names of any two of the Graeae., TWO OF: ENYO CC PE(M)PHREDO CC OR DEINO", Mythology, YEAR , CONTEST, LEVEL
--------
-
-    """
-        f"Please analyze this for tossup question {question} for round {ROUND} as a row divided by commas; REPLACE ALL COMMAS IN ALL QUESTIONS (BUT NOT THE COMMAS SEPARATING THE PARTS OF THE QUESTION) with 'CC' in the format of the following, also try to remove the labels of the questions, such as 'B1' or '1' :\n"
-        "Main Question, Main Question Answer, Bonus Question 1, Bonus Question 1 Answer, Bonus Question 2, Bonus Question 2 Answer, Difficulty Level, Topic, Year, Contest Name, Round\n"
-        "NOTE: if the bonus doesn't exist, return 'None'"
+        f"""
+        -----
+        
+        Here is a formatted EXAMPLE QUESTION: 
+        From what Latin verb with what meaning are the English words “adroitCC” “incorrigibleCC” and “regiment” derived?, REGŌ (REGERE ) = (TO / I) RULE, From what Latin verb with what meaning is “surrogate” derived?, ROGŌ (ROGĀRE) = (TO / I) ASK, From what Latin verb with what meaning is “risible” derived?, RĪDEŌ (RĪDĒRE) = (TO / I) LAUGH, Grammar Derivatives, YEAR, CONTEST, LEVEL
+        
+        -------
+        
+        Please analyze and output the correct format this question for tossup question {question} for round {ROUND} as a row divided by commas; REPLACE ALL COMMAS IN ALL QUESTIONS (BUT NOT THE COMMAS SEPARATING THE PARTS OF THE QUESTION) with 'CC' in the format of the following, also try to remove the labels of the questions, such as 'B1' or '1':\n
+        
+        Main Question, Main Question Answer, Bonus Question 1, Bonus Question 1 Answer, Bonus Question 2, Bonus Question 2 Answer, Difficulty Level, Topic, Year, Contest Name, Round\n"""
     )
 
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
-            {"role": "system", "content": context},
+            {"role": "user", "content": context},
             {"role": "user", "content": assistant_prompt},
             {"role": "user", "content": text.replace("\n", " ")},
         ],
     )
-    assistant_prompt = (
-        """
-        -----
-        Here is a formatted EXAMPLE QUESTION: 
-        From what Latin verb with what meaning are the English words “adroitCC” “incorrigibleCC” and “regiment” derived?, REGŌ (REGERE ) = (TO / I) RULE, From what Latin verb with what meaning is “surrogate” derived?, ROGŌ (ROGĀRE) = (TO / I) ASK, From what Latin verb with what meaning is “risible” derived?, RĪDEŌ (RĪDĒRE) = (TO / I) LAUGH, Grammar Derivatives, YEAR, CONTEST, LEVEL
-        -------
-    """
-        f"Please analyze this for tossup question {question} for round {ROUND} as a row divided by commas; REPLACE ALL COMMAS IN ALL QUESTIONS (BUT NOT THE COMMAS SEPARATING THE PARTS OF THE QUESTION) with 'CC' in the format of the following, also try to remove the labels of the questions, such as 'B1' or '1' :\n"
-        "Main Question, Main Question Answer, Bonus Question 1, Bonus Question 1 Answer, Bonus Question 2, Bonus Question 2 Answer, Difficulty Level, Topic, Year, Contest Name, Round\n"
-    )
+
     r = response.choices[0].message.content
 
     print()
-    print(f"Question {question}")
+    print(f"---- Question {question} ----")
     if r or r != "None":
+        initial_model = "gpt-4o"
+        print(f"Performing initial formatting prompt with {initial_model}")
         response = client.chat.completions.create(
-            model="gpt-4o",
+            model=initial_model,
             messages=[
                 {"role": "system", "content": context},
                 {"role": "user", "content": assistant_prompt},
                 {
                     "role": "user",
-                    "content": f"Remember to reformat as needed to match the format of the examples, reducing the number of split parts equals 11, currently there are {len(response.choices[0].message.content.split(','))}",
+                    "content": f"Remember to reformat as needed to match the format of the examples, reducing the number of split parts equals 11, currently there are {len(response.choices[0].message.content.split(','))}. Output so that this data can be used to create a row in a CSV file – do not give anything other than the data needed to create the row.",
                 },
                 {"role": "user", "content": response.choices[0].message.content},
             ],
         )
-        r = response.choices[0].message.content
+        origional_output = response.choices[0].message.content
 
         options = [
             a.replace("CC", ",").replace("\n", " ").replace("  ", " ").strip()
-            for a in r.strip().split(", ")
+            for a in origional_output.strip().split(", ")
         ]
         if len(options) != len(df.columns):
+            print(f"Wrong number of options ({len(options)}), retrying with o1-preview")
             response = client.chat.completions.create(
                 model="o1-preview",
                 messages=[
-                    {"role": "system", "content": context},
-                    {"role": "user", "content": assistant_prompt + f"Remember to reformat as needed to match the format of the examples, REDUCE the number of split parts, which was at {len(options)} which is NOT EQUAL to {len(response.choices[0].message.content.split(','))}" +   response.choices[0].message.content},
-                    {"role": "user", "content": "DO NOT MENTION THE REFORMATTING IN THE OUTPUT, OR ANYTHING ELSE BESIDES THE QUESTION"},
+                    {"role": "user", "content": context + "\n"},
+                    {
+                        "role": "user",
+                        "content": assistant_prompt
+                        + f"Remember to output the response as reformated as needed to match the format of the examples, REDUCE the number of split parts, which was at {len(options)} which is NOT EQUAL to {len(response.choices[0].message.content.split(','))}"
+                        + response.choices[0].message.content,
+                    },
                 ],
             )
-            r = response.choices[0].message.content
+            revised_response = response.choices[0].message.content
             options = [
                 a.replace("CC", ",").replace("\n", " ").replace("  ", " ").strip()
-                for a in r.strip().split(", ")
+                for a in revised_response.strip().split(", ")
             ]
-        print(options)
-        print("Option Length: ", len(options))
+            if len(options) != len(df.columns):
+                print(
+                    f"The wrong number of options (with length of {len(options)} compared to the correct length of {len(df.columns)}) was returned, skipping and writing question into a file"
+                )
+                with open("Incorrect_Questions.txt", "a") as f:
+                    f.write(
+                        f"Question {question} of round {ROUND} from pdf {pdf_path} is incorrect: {revised_response}\n"
+                    )
+                continue
+
         for i in range(len(options)):
             options[i] = options[i].replace("CC", ",")
-
+        options[-1] = ROUND
+        print("Option Length: ", len(options))
         print("Options: ", options)
-        print("Length of options: ", len(options))
 
         print("Length of df.columns: ", len(df.columns))
         if len(options) == len(df.columns):
